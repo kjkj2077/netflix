0b4150cad840e371b3dc76db2f2d9d159544f3eb
고진규(KOJINKYU) <71066140+kjkj2077@users.noreply.github.com>
2022-08-08 00:55:39 +0900

    Update MovieCard2.js

--- src/components/MovieCard2.js
+++ src/components/MovieCard2.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 export const MovieCard2 = ({movie}) => {
     const url2 = `https://image.tmdb.org/t/p/original///${movie.poster_path}`
-   // const navigate = useNavigate()
+    const navigate = useNavigate()
     const showDetail = () => {
-        //navigate(`/movies/${movie.id}`)
-        window.location.replace(`/movies/${movie.id}`)
+        navigate(`/movies/${movie.id}`)
+        window.location.reload();
     }
     return (   
         <div className="card" style={{backgroundColor:"black"}} onClick={showDetail} >
